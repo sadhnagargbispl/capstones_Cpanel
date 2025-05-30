42072bf3d1348934b5cbd886c484c3c746d1558a
hemrajsharma1987 <80751679+hemrajsharma1987@users.noreply.github.com>
2025-05-30 10:33:13 +0530

    30 May 2025
    ss

--- Directlogin.aspx.cs
+++ Directlogin.aspx.cs
@@ -25,89 +25,112 @@ partial class Directlogin : System.Web.UI.Page
     {
         try
         {
-            string formPostText = "";
-            string UserName = Encrypt(Session["IDNO"].ToString());
-            string Password = Encrypt(Session["MemPassw"].ToString());
+            if (Session["IDNO"] == null)
+                Response.Redirect("https://dxmall.in/");
+            else
+            {
+                //var LgnID = Encrypt("uid=" + Session["IDNo"] + "&pwd=" + Session["MemPassw"]);
+                //DateTime currentDate = DateTime.Now;
+                //string TmID = currentDate.Day.ToString("00") + currentDate.Hour.ToString("00") + currentDate.Year.ToString() + (currentDate.Month - 1).ToString();
+                //string url = "https://dgtalexpo.com/Default.aspx?lgnT=" + LgnID + "&ID=" + TmID + "";
+                ////string url = "http://localhost:59233/Default.aspx?lgnT=" + LgnID + "&ID=" + TmID + "";
+                //Response.Redirect(url);
+                string formPostText = "";
+                string UserName = Encrypt(Session["IDNO"].ToString());
+                string Password = Encrypt(Session["MemPassw"].ToString());
 
-                formPostText = @"<form method=""POST"" action=""https://shop.capstones.in/Account/DirectLogin"" name=""frm2Post"">" +
-                               " <input type=\"hidden\" name=\"LoginId\" value=\"" + UserName + "\" />" +
-                               " <input type=\"hidden\" name=\"Password\" value=\"" + Password + "\" />" +
-                               " <input type=\"hidden\" name=\"Token\" value=\"9w8gfOY/3amGrCPm1cOAhscvtuPoPqHombKOhTRRhRjDaSj3ppzsZG/gZgl1DnU2\" />" +
-                               " <script type=\"text/javascript\">document.frm2Post.submit();</script></form>";
+                string Token = "ODIzNTEzQzgtNzVDOS00RjE3LUIzOTYtMDlERDQzOUQ0Qjcy";
+                //string info12 = "UserName=" + Convert.ToString(Session["IDNO"]) + "&Password=" + Convert.ToString(Session["MemPassw"]) + "&Action=Login" + "&Token=" + Token;
+                string info12 = "UserName=" + Session["IDNO"] + "&Password=" + Session["MemPassw"] + "&Action=Login" + "&Token=" + Token;
+                string refes = Base64Encode(info12);
+                string data = Base64Decode(refes);
+                Response.Redirect("https://dxmall.in/Account/DirectLogin?URL=" + refes);
+            }
 
-                Response.Write(formPostText);
+
+            //string formPostText = "";
+            //string UserName = Encrypt(Session["IDNO"].ToString());
+            //string Password = Encrypt(Session["MemPassw"].ToString());
+
+            //    formPostText = @"<form method=""POST"" action=""https://shop.capstones.in/Account/DirectLogin"" name=""frm2Post"">" +
+            //                   " <input type=\"hidden\" name=\"LoginId\" value=\"" + UserName + "\" />" +
+            //                   " <input type=\"hidden\" name=\"Password\" value=\"" + Password + "\" />" +
+            //                   " <input type=\"hidden\" name=\"Token\" value=\"9w8gfOY/3amGrCPm1cOAhscvtuPoPqHombKOhTRRhRjDaSj3ppzsZG/gZgl1DnU2\" />" +
+            //                   " <script type=\"text/javascript\">document.frm2Post.submit();</script></form>";
+
+            //    Response.Write(formPostText);
         }
         catch (Exception ex)
         {
         }
     }
-    //public static byte[] MD5Hash(string value)
-    //{
-    //    return MD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(value));
-    //}
-    //public static string Encrypt(string stringToEncrypt)
-    //{
-    //    DES.Key = Crypto.MD5Hash(key);
-    //    DES.Mode = CipherMode.ECB;
-    //    byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(stringToEncrypt);
-    //    return Convert.ToBase64String(DES.CreateEncryptor().TransformFinalBlock(Buffer, 0, Buffer.Length));
-    //}
-    //public static string Decrypt(string encryptedString)
-    //{
-    //    try
-    //    {
-    //        DES.Key = Crypto.MD5Hash(key);
-    //        DES.Mode = CipherMode.ECB;
-    //        byte[] Buffer = Convert.FromBase64String(encryptedString);
-    //        return ASCIIEncoding.ASCII.GetString(DES.CreateDecryptor().TransformFinalBlock(Buffer, 0, Buffer.Length));
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        return DBNull.Value.ToString();
-    //    }
-    //}
-    public string Encrypt(string plainText)
+    public static byte[] MD5Hash(string value)
     {
-        string completeUrl = "https://shop.capstones.in/Account/Encrypt?plainText=" + plainText;
-        System.Net.ServicePointManager.SecurityProtocol = (System.Net.SecurityProtocolType)3072; // TLS 1.2
-
-        HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(completeUrl);
-        request1.ContentType = "application/json";
-        request1.Method = "GET";
-
-        using (HttpWebResponse httpWebResponse = (HttpWebResponse)request1.GetResponse())
-        using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+        return MD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(value));
+    }
+    public static string Encrypt(string stringToEncrypt)
+    {
+        DES.Key = Crypto.MD5Hash(key);
+        DES.Mode = CipherMode.ECB;
+        byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(stringToEncrypt);
+        return Convert.ToBase64String(DES.CreateEncryptor().TransformFinalBlock(Buffer, 0, Buffer.Length));
+    }
+    public static string Decrypt(string encryptedString)
+    {
+        try
         {
-            string responseString = reader.ReadToEnd();
-            return responseString;
+            DES.Key = Crypto.MD5Hash(key);
+            DES.Mode = CipherMode.ECB;
+            byte[] Buffer = Convert.FromBase64String(encryptedString);
+            return ASCIIEncoding.ASCII.GetString(DES.CreateDecryptor().TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+        catch (Exception ex)
+        {
+            return DBNull.Value.ToString();
         }
     }
-    //private string Base64Encode(string plainText)
+    //public string Encrypt(string plainText)
     //{
-    //    var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-    //    return System.Convert.ToBase64String(plainTextBytes);
+    //    string completeUrl = "https://shop.capstones.in/Account/Encrypt?plainText=" + plainText;
+    //    System.Net.ServicePointManager.SecurityProtocol = (System.Net.SecurityProtocolType)3072; // TLS 1.2
+
+    //    HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(completeUrl);
+    //    request1.ContentType = "application/json";
+    //    request1.Method = "GET";
+
+    //    using (HttpWebResponse httpWebResponse = (HttpWebResponse)request1.GetResponse())
+    //    using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+    //    {
+    //        string responseString = reader.ReadToEnd();
+    //        return responseString;
+    //    }
     //}
-    //private string Base64Decode(string base64EncodedData)
-    //{
-    //    var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
-    //    return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-    //}
-    public string Decrypt(string cipherText)
+    private string Base64Encode(string plainText)
     {
-        byte[] iv = new byte[16]; // 16-byte IV for AES
-        byte[] buffer = Convert.FromBase64String(cipherText);
-
-        using (Aes aes = Aes.Create())
-        {
-            aes.Key = Encoding.UTF8.GetBytes("Y$8tM9d#k4KqpV^rLw2zXN&yS7uPqU@3"); // 32-byte key for AES-256
-            aes.IV = iv;
-
-            using (MemoryStream ms = new MemoryStream(buffer))
-            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
-            using (StreamReader reader = new StreamReader(cs))
-            {
-                return reader.ReadToEnd();
-            }
-        }
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
     }
+    private string Base64Decode(string base64EncodedData)
+    {
+        var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+    }
+    //public string Decrypt(string cipherText)
+    //{
+    //    byte[] iv = new byte[16]; // 16-byte IV for AES
+    //    byte[] buffer = Convert.FromBase64String(cipherText);
+
+    //    using (Aes aes = Aes.Create())
+    //    {
+    //        aes.Key = Encoding.UTF8.GetBytes("Y$8tM9d#k4KqpV^rLw2zXN&yS7uPqU@3"); // 32-byte key for AES-256
+    //        aes.IV = iv;
+
+    //        using (MemoryStream ms = new MemoryStream(buffer))
+    //        using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+    //        using (StreamReader reader = new StreamReader(cs))
+    //        {
+    //            return reader.ReadToEnd();
+    //        }
+    //    }
+    //}
 }
--- ShoppingRedirect.aspx.cs
+++ ShoppingRedirect.aspx.cs
@@ -26,14 +26,25 @@ public partial class ShoppingRedirect : System.Web.UI.Page
             {
                 try
                 {
+                    //string formPostText = "";
+                    //formPostText = "<form method=\"POST\" action=\"https://shopping.capstones.in/members/index.php\" name=\"frm2Post\">";
+                    //formPostText += "<input type=\"hidden\" name=\"token\" value=\"bce077f7ce2069e73a6d1c99a4e9f4ff\" />";
+                    //formPostText += "<input type=\"hidden\" name=\"mod\" value=\"interLogin\" />";
+                    //formPostText += "<input type=\"hidden\" name=\"userid\" value=\"" + Session["IDNO"].ToString() + "\" />";
+                    //formPostText += "<input type=\"hidden\" name=\"password\" value=\"" + Session["MemPassw"].ToString() + "\" /> ";
+                    //formPostText += "<script type=\"text/javascript\">document.frm2Post.submit();</script>";
+                    //formPostText += "</form>";
+                    //Response.Write(formPostText);
                     string formPostText = "";
-                    formPostText = "<form method=\"POST\" action=\"https://shopping.capstones.in/members/index.php\" name=\"frm2Post\">";
-                    formPostText += "<input type=\"hidden\" name=\"token\" value=\"bce077f7ce2069e73a6d1c99a4e9f4ff\" />";
-                    formPostText += "<input type=\"hidden\" name=\"mod\" value=\"interLogin\" />";
-                    formPostText += "<input type=\"hidden\" name=\"userid\" value=\"" + Session["IDNO"].ToString() + "\" />";
-                    formPostText += "<input type=\"hidden\" name=\"password\" value=\"" + Session["MemPassw"].ToString() + "\" /> ";
-                    formPostText += "<script type=\"text/javascript\">document.frm2Post.submit();</script>";
-                    formPostText += "</form>";
+                    string UserName = Encrypt(Session["IDNO"].ToString());
+                    string Password = Encrypt(Session["MemPassw"].ToString());
+
+                    formPostText = @"<form method=""POST"" action=""https://shop.capstones.in/Account/DirectLogin"" name=""frm2Post"">" +
+                                   " <input type=\"hidden\" name=\"LoginId\" value=\"" + UserName + "\" />" +
+                                   " <input type=\"hidden\" name=\"Password\" value=\"" + Password + "\" />" +
+                                   " <input type=\"hidden\" name=\"Token\" value=\"9w8gfOY/3amGrCPm1cOAhscvtuPoPqHombKOhTRRhRjDaSj3ppzsZG/gZgl1DnU2\" />" +
+                                   " <script type=\"text/javascript\">document.frm2Post.submit();</script></form>";
+
                     Response.Write(formPostText);
                 }
                 catch (Exception ex)
@@ -47,6 +58,40 @@ public partial class ShoppingRedirect : System.Web.UI.Page
             throw new Exception(ex.Message);
         }
 
+    }
+    public string Encrypt(string plainText)
+    {
+        string completeUrl = "https://shop.capstones.in/Account/Encrypt?plainText=" + plainText;
+        System.Net.ServicePointManager.SecurityProtocol = (System.Net.SecurityProtocolType)3072; // TLS 1.2
+
+        HttpWebRequest request1 = (HttpWebRequest)WebRequest.Create(completeUrl);
+        request1.ContentType = "application/json";
+        request1.Method = "GET";
+
+        using (HttpWebResponse httpWebResponse = (HttpWebResponse)request1.GetResponse())
+        using (StreamReader reader = new StreamReader(httpWebResponse.GetResponseStream()))
+        {
+            string responseString = reader.ReadToEnd();
+            return responseString;
+        }
+    }
+    public string Decrypt(string cipherText)
+    {
+        byte[] iv = new byte[16]; // 16-byte IV for AES
+        byte[] buffer = Convert.FromBase64String(cipherText);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes("Y$8tM9d#k4KqpV^rLw2zXN&yS7uPqU@3"); // 32-byte key for AES-256
+            aes.IV = iv;
+
+            using (MemoryStream ms = new MemoryStream(buffer))
+            using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Read))
+            using (StreamReader reader = new StreamReader(cs))
+            {
+                return reader.ReadToEnd();
+            }
+        }
     }
     //public string Encrypt(string plainText)
     //{
